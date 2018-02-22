0d59c0c838679f8a0a088e74eb9ae88760920c4f
hat_11_hat <ken.chana9@gmail.com>
2018-02-22 18:46:03 +0900

    added new buttons.

--- src/hat0mam3/orga/OrgaController.java
+++ src/hat0mam3/orga/OrgaController.java
@@ -2,6 +2,8 @@ package hat0mam3.orga;
 
 import java.io.File;
 
+import org.ietf.jgss.Oid;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,11 +11,27 @@ import javafx.scene.media.AudioClip;
 
 public class OrgaController {
 	@FXML
-	private Button button;
+	private Button buttonNonStop;
+	@FXML
+	private Button buttonEisho;
+	@FXML
+	private Button buttonNonRelation;
 	
 	@FXML
-	public void onClick(ActionEvent aEvent){
+	public void onClickNonStop(ActionEvent aEvent){
 		AudioClip aClip = new AudioClip(new File("./orga.wav").toURI().toString());
+		aClip.play();
+	}
+	
+	@FXML
+	public void onClickEisho(ActionEvent aEvent){
+		AudioClip aClip = new AudioClip(new File("./eisho.wav").toURI().toString());
+		aClip.play();
+	}
+	
+	@FXML
+	public void onClickNonRelation(ActionEvent aEvent){
+		AudioClip aClip = new AudioClip(new File("./ride.wav").toURI().toString());
 		aClip.play();
 	}
 }
