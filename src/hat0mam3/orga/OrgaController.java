1725fd49e8bee3e8eba7c89edd26006dd2b549c1
hat_11_hat <ken.chana9@gmail.com>
2018-02-26 01:37:41 +0900

    WIP

--- src/hat0mam3/orga/OrgaController.java
+++ src/hat0mam3/orga/OrgaController.java
@@ -1,6 +1,9 @@
 package hat0mam3.orga;
 
 import java.io.File;
+
+import javax.security.auth.login.FailedLoginException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -15,29 +18,30 @@ public class OrgaController {
 	private Button buttonEisho;
 	@FXML
 	private Button buttonNonRelation;
-	
+
 	@FXML
 	public void onClickDontStop(ActionEvent aEvent) {
 		playMusic("./orga.wav");
 	}
-	
+
 	@FXML
 	public void onClickNonStop(ActionEvent aEvent){
 		playMusic("./nonstop.wav");
 	}
-	
+
 	@FXML
 	public void onClickEisho(ActionEvent aEvent){
 		playMusic("./eisho.wav");
 	}
-	
+
 	@FXML
 	public void onClickNonRelation(ActionEvent aEvent){
 		playMusic("./ride.wav");
 	}
-	
+
 	public void playMusic(String fileName){
 		AudioClip audioClip = new AudioClip(new File(fileName).toURI().toString());
 		audioClip.play();
 	}
+
 }
